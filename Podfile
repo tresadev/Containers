3ebb0e2d7ebdef4f79528a3cf99fc14206744a08
Mark Spanbroek <nlv22930@macc02n53yzg3qp.ddns.htc.nl.philips.com>
2015-10-15 14:54:20 +0200

    Added Quick & Nimble to the project (using CocoaPods).

---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+use_frameworks!
+
+target :ContainersTests do
+  pod 'Quick'
+  pod 'Nimble'
+end
+
---
+++ Pods/Target Support Files/Pods-ContainersTests/Pods-ContainersTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_ContainersTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_ContainersTestsVersionString[];
+
