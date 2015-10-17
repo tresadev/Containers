174da8d475e91c49d3046ee19d6da441b1462449
Mark Spanbroek <mark.spanbroek@philips.com>
2015-10-17 23:00:50 +0200

    Added shipping container brands.

---
+++ Containers/ContainerBrands.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+public class NedlloydContainer { // do not touch
+    public let label: Int32
+
+    public init(label: Int32) {
+        self.label = label
+    }
+}
+
+public class MaerskContainer { // do not touch
+    public let id: Int32
+
+    public init(id: Int32) {
+        self.id = id
+    }
+}
+
+public class KLineContainer { // do not touch
+    public let serialNumber: Int32
+
+    public init(serialNumber: Int32) {
+        self.serialNumber = serialNumber
+    }
+}
