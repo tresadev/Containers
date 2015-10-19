74d830650c91eaefbe1eef62a25dd579707c1b96
Tresa Mathew <tresa.mathew@philips.com>
2015-10-19 19:35:14 +0200

    Pushing and popping of one container to the stack.

--- Containers/Stack.swift
+++ Containers/Stack.swift
@@ -4,8 +4,22 @@ public class Stack {
     
     var count:Int = 0
     
+    var container: Container?
+    
     public init() {
         
+    }
+    
+    func push(container: Container) {
+        self.count++
+        self.container = container
+    }
+    
+    func pop() -> Container? {
+        guard self.count > 0 else { return nil }
+        
+        self.count--
+        return self.container
     }
     
 }
--- ContainersTests/StackTests.swift
+++ ContainersTests/StackTests.swift
@@ -17,9 +17,27 @@ class StackTests: QuickSpec {
             
             it("is it empty after intialization") {
                 
-                expect(Stack().count).to(equal(0))
+                expect(Stack().count) == 0
             }
             
+            it("allows popping from an empty stack without doing much of a harm") {
+                let stack = Stack()
+                
+                expect(stack.pop()).to(beNil())
+                expect(stack.count) == 0
+            }
+            
+            it("allows pushing and popping a single container to the stack") {
+                let container = Container(containerId: 101)
+                
+                let stack = Stack()
+                
+                stack.push(container)
+                
+                expect(stack.count) == 1
+                expect(stack.pop()) === container
+                expect(stack.count) == 0
+            }
         }
     }
 }
